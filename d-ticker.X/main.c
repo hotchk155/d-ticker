c383c31fdb6c9160bad0bfbbe8d0f28218fa0eac
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2024-12-12 10:16:45 +0000

    dsfsdf

--- d-ticker.X/main.c
+++ d-ticker.X/main.c
@@ -1,11 +1,3 @@
-/*
- * File:   main.c
- * Author: jason
- *
- * Created on 28 November 2024, 14:30
- */
-
-
 
 // PIC16F1825 Configuration Bit Settings
 
@@ -37,7 +29,33 @@
 #include <xc.h>
 
 typedef unsigned char byte;
-#include "tan.h"
+
+enum {    
+    MAX_INPUT_STEPS = 16,
+    MAX_OUTPUT_RATE = 4,
+    MAX_TRIGS = (MAX_INPUT_STEPS * MAX_OUTPUT_RATE)
+};
+
+float tt[MAX_TRIGS];
+
+float g_trig[MAX_TRIGS];
+int g_num_trigs;
+int e_num_input_steps;
+int e_output_rate;
+int g_trigs_per_section;
+
+enum {
+    INPUT_STEPS_4,
+    INPUT_STEPS_8,
+    INPUT_STEPS_16,
+    INPUT_STEPS_32
+};
+enum {
+    OUTPUT_RATE_DIV2,
+    OUTPUT_RATE_X1,
+    OUTPUT_RATE_X2,
+    OUTPUT_RATE_X4
+};
 
 
 /*
@@ -121,18 +139,11 @@ volatile byte reset_in_signal = 0;
 #define CLOCK_LED_TIMEOUT 10
 volatile byte clock_led_timeout = 0;
 
-#define SZ_TEMPO_MAP 16
-int tempo_map[SZ_TEMPO_MAP];
+#define OUTPUT_PULSE_MS 10
+#define OUTPUT_PULSE_LOW_MS 5
+volatile byte trig_out_count = 0;
+volatile byte trig_out_timeout = 0;
 
-// define the time base used for scheduling output triggers...
-// this will divide the full bar into 65536 units
-#define MAX_TICKS 0xFFFF
-typedef unsigned int TICKS;
-volatile TICKS cur_ticks = 0;
-
-// The schedule for each tick
-#define SZ_TRIG_SCHEDULE 16
-TICKS trig_schedule[SZ_TRIG_SCHEDULE];
 
 void adc_begin() {
 	switch(which_pot) {
@@ -174,9 +185,6 @@ void __interrupt() ISR()
 				prev_clock_in = 1;
 				clock_in_signal = 1;
 				debounce_clock_in = DEBOUNCE_CLOCK_IN;
-				
-P_CLOCKLED = 1;
-clock_led_timeout = CLOCK_LED_TIMEOUT;
 			}
 		}
 		else {
@@ -204,6 +212,21 @@ clock_led_timeout = CLOCK_LED_TIMEOUT;
 				P_CLOCKLED = 0;
 			}
 		}
+        
+        // manage trig output
+        if(trig_out_timeout) {
+            --trig_out_timeout;
+            if(trig_out_timeout <= OUTPUT_PULSE_LOW_MS) {
+                P_CLOCKOUT = 0;
+            }
+        }
+        else if(trig_out_count) {
+            --trig_out_count;
+            P_CLOCKOUT = 1;
+            trig_out_timeout = (OUTPUT_PULSE_MS + OUTPUT_PULSE_LOW_MS);
+        }
+        
+                
 		INTCONbits.T0IF = 0;
 	}
 	
@@ -263,135 +286,56 @@ void set_led(int i) {
 // Recalculate the tempo map
 /////////////////////////////////////////////////////////////////////////////
 void recalc() {
+    
 
-	// We read each of the four potentiometers as an 8 bit value
-	// -127 to +127 which is treated as an angle -45 to +45 degrees
-	// for one of four straight line sections running end to end from
-	// y-position zero
-	//
-	//          |_________|         |
-	//         /|         |---      |
-	//       /  |         |   ---   |       
-	//     /    |         |      ---|	___---
-	//   /      |         |         |---
-	// /        |         |         |
-	// ----+----|----+----|----+----|----+----
-	//  256 256 | 256 256 | 256 256 | 256 256
-	//
-	// 
-	int i;
-	// calculate the y-displacement along each of the four line
-	// sections, based on pot input and 512 unit width
-	int dy[4];
-	int base_dy = 0;
-	for(i=0; i<4; ++i) {	
-		int q = pot[i];
-//uart_send_string(" //q=");		
-//uart_send_number(q);		
-		int t;
-		int index;
-		if(q>127) { // positive angle/increasing tempo
-			index = q-128;
-			t =  (byte)tan_table[index];
-		}
-		else { // negative angle/dec reasing tempo
-			index = 127-q;
-			t =  -(byte)tan_table[index];
-		}		
-		
-//uart_send_string(" index=");		
-//uart_send_number(index);		
-//uart_send_string(" t=");		
-//uart_send_number(t);		
-		dy[i] = 2*t;
-//uart_send_string(" dy=");		
-//uart_send_number(dy[i]);		
-//uart_send_string(" ");		
-	}
-//uart_send_string("\r\n");		
-	
-	// calculate the area under the graph.. maximum area is 1/2(0 + 2048)2048 = 0x200000
-	// which corresponds to all four pots being at the same full extent
-	int y1 = 0;
-	long area = 0;
-	for(i=0; i<4; ++i) {	
-		int y2 = y1 + dy[i];
-		area += ((long)256*(y1+y2)); // 1/2(a+b)h
-		y1=y2;
-	}
-//uart_send_string("area=");		
-//uart_send_long(area);		
-//uart_send_string("\r\n");		
-	
-	// now calculate the y offset needed to all the points so that the area under
-	// the graph would be zero. To do this, we'll take half the area under the graph
-	// and divide it by length of the x-axis (2048). If the graph is then offset down
-	// the y-axis by this value the area under the graph will be zero
-	int y_offset = -area/2048;
-
-//uart_send_string("offset=");		
-//uart_send_long((long)y_offset);		
-//uart_send_string("\r\n");		
-	
-	// now we can build the tempo map... each element in this array represents an offset of 
-	// the input BPM to be applied during that step of the map	
-	for(int i=0; i<SZ_TEMPO_MAP; ++i) {
-		int section = ((i/4)%4);
-		
-		// for this tempo map slot we'll use the average y-value along the tempo line section
-		// within the current "slot". There are 4 slots per section (each 128 units wide)
-		// so we look at the midpoint of the slot (64 units in, or 1/8 of the full y offset
-		// over the whole slot
-		// 
-		//
-		// |           ----|
-		// |       ----    |
-		// |   ----        |
-		// |---            |
-		// |  256  |  256  |
-		// |128|128|128|128|
-		// | |  
-		
-		tempo_map[i] = y_offset + dy[section]/8;
-		y_offset = y_offset + dy[section]/4;		
-//uart_send_string("tempo[");		
-//uart_send_number(i);		
-//uart_send_string("]=");		
-//uart_send_number(tempo_map[i]);		
-//uart_send_string("\r\n");		
-		
-	}
+    g_num_trigs = 16; //TODO
+    
+    
+    // expand out the velocity(tempo) changes (defined by the pots) into a 
+    // list of velocity values at each output trigger position
+    float cur_rate = 1.0;
+    float min_rate = 1.0;
+    int trig;
+    for(trig=0; trig<g_num_trigs; ++trig) {
+        tt[trig] = cur_rate;        
+        float acc = (float)(1.0-pot[(trig*4)/g_num_trigs]/128.0);                
+        cur_rate += acc;
+        if(cur_rate < min_rate) {
+            min_rate = cur_rate;
+        }        
+    }
+    
+    // normalise the velocities so that they are all positive and 
+    // expand out the "distance into sequence" by integrating velocity
+    float dist = 0.0;
+    for(trig=0; trig<g_num_trigs; ++trig) {
+        float norm_rate = (float)(tt[trig] - min_rate + 1.0);
+        tt[trig] = dist;
+        dist = dist + norm_rate;
+    }
+    
+    // now normalise the distances so that they run from 0.0 .. 1.0
+    for(trig=0; trig<g_num_trigs; ++trig) {
+        g_trig[trig] = tt[trig] / dist;
+    }
 }
 
-
+void xrecalc() {
+   g_num_trigs = 16;
+   for(int i=0; i<16; ++i) {
+       g_trig[i] = (float)(i * (1.0/16.0));
+   }
+}
 
 
 
 void test_pots() {
 	for(;;) {
-		int j = pot[0];
+		int j = pot[3];
 		set_led(j/64);
 		//set_led(0);
 	}
 }
-
-void test_leds() {
-    int count = 0;
-    for(;;) {
-        set_led(count++%4);        
-        for(int i=0; i<500; ++i) {
-            ms_tick = 0;
-            while(!ms_tick);
-            P_CLOCKOUT = count&1;
-            P_CLOCKLED = count&1;
-        }
-    }
-}	
-void test_inputs() {
-    for(;;) {
-        P_CLOCKLED = P_SWITCH;
-    }
-}	
 
 // MAIN
 void main()
@@ -408,46 +352,20 @@ void main()
 	ANSELA = 0b00000100;  
 	ANSELC = 0b00000111;
 	PORTA=0;
-	PORTC=0;
-
-   // configure analog input AN2 enabled
-  
-        // turn on the ADC
-        // ADC clock is Fosc/32
-        // Result left justified (8 bit value in adresh register)
-        // Voltage reference is power supply (VDD)
+	PORTC=0; 
+    WPUA = WPUA_BITS;
+    WPUC = 0;
+    
+    // turn on the ADC
+    // ADC clock is Fosc/32
+    // Result left justified (8 bit value in adresh register)
+    // Voltage reference is power supply (VDD)
         ADCON1=0b00100000; //fOSC/32
-  //      adcon0=0b00001001; 
-
-
-/*
-	// Configure timer 1 (controls tempo)
-	// Input 4MHz
-	// Prescaled to 500KHz
-	tmr1l = 0;	 // reset timer count register
-	tmr1h = 0;
-	t1con.7 = 0; // } Fosc/4 rate
-	t1con.6 = 0; // }
-	t1con.5 = 1; // } 1:8 prescale
-	t1con.4 = 1; // }
-	t1con.0 = 1; // timer 1 on
-	pie1.0 = 1;  // timer 1 interrupt enable
-	*/
-	
 	// Configure timer 0 (controls systemticks)
 	// 	timer 0 runs at 4MHz
 	// 	prescaled 1/16 = 250kHz
 	// 	rollover at 250 = 1kHz
-	// 	1ms per rollover	
-	//option_reg.5 = 0; // timer 0 driven from instruction cycle clock
-	//option_reg.3 = 0; // timer 0 is prescaled
-	//option_reg.2 = 0; // }
-	//option_reg.1 = 1; // } 1/16 prescaler
-	//option_reg.0 = 1; // }
- 
-    WPUA = WPUA_BITS;
-    WPUC = 0;
-    
+	// 	1ms per rollover	    
     OPTION_REGbits.TMR0CS = 0;  // timer 0 driven from instruction cycle clock
     OPTION_REGbits.PSA = 0;     // timer 0 is prescaled
     OPTION_REGbits.PS = 0b011;  // 1/16 prescaler
@@ -455,26 +373,59 @@ void main()
     
     INTCONbits.T0IE = 1;    // enabled timer 0 interrrupt
     INTCONbits.T0IF = 0;    // clear interrupt fired flag
-	//intcon.5 = 1; 	  // enabled timer 0 interrrupt
-	//intcon.2 = 0;     // clear interrupt fired flag
 
     PIR1bits.ADIF = 0;
     PIE1bits.ADIE = 1; // enable the ADC interrupt
-	//pir1.6 = 0;
-	//pie1.6 = 1; // enable the ADC interrupt
 	
-	//uart_init();
-	// enable interrupts	
-	//intcon.7 = 1; //GIE
-	//intcon.6 = 1; //PEIE
     INTCONbits.GIE = 1;
     INTCONbits.PEIE = 1;
 
-    test_inputs();
-    
 	// poll pots
 	adc_begin();
 	
+    
+    
+ 
+    //e_num_input_steps = INPUT_STEPS_16;
+    //e_output_rate = OUTPUT_RATE_X1;
+    recalc();
+    
+    int led_count = 0;
+    int cur_trig = 0;
+    float timer = 0.0;
+    float rate = 1.0/8000.0;    
+    int refresh_count = 0;
 	for(;;) {
+        if(ms_tick) {
+            ms_tick = 0;
+            if(!refresh_count) {
+                recalc();
+                refresh_count = 1000;
+            }
+            --refresh_count;
+                //recalc();
+            //}            
+            while(cur_trig<g_num_trigs) {
+                if(g_trig[cur_trig] > timer) {
+                    break;
+                }
+                set_led((4*cur_trig)/g_num_trigs);
+                //P_CLOCKLED = 1;
+                led_count = 10;
+                ++cur_trig;
+            }
+            timer += rate;
+            if(timer >= 1.0) {
+                timer = 0.0;
+                cur_trig = 0;
+            }
+            
+            if(led_count) {
+                if(!--led_count) {
+                    P_CLOCKLED = 0;
+                    set_led(-1);
+                }
+            }
+        }
 	}
 }
